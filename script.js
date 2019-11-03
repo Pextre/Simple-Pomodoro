dde5f0727da4b3740fb5d96431d73f473e0f3ad4
Daniel Peixoto Pinto da Silva <peixoto.daniel@hotmail.com>
2019-11-02 22:04:23 -0300

    Correção de alguns erros
    Correção de alguns erros encontrados pela ferramenta Codacy

--- script.js
+++ script.js
@@ -2,43 +2,20 @@ let currentTimer;
 let active = false;
 let interval = false;
 
-function timer(duration, display) {
+function stopTimer(stop = false) {
 
-  currentTimer = setInterval(function () {
-    minutes = parseInt(duration / 60, 10);
-    seconds = parseInt(duration % 60, 10);
+  if (stop) {
+    clearInterval(currentTimer);
+    active = false;
+  }
 
-    duration--;
-
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-
-    if (duration === -1 && !interval) {
-
-      document.querySelector('audio').play();
-      stopTimer(true);
-      interval = true;
-      active = true;
-      timer(900,display);// descanso de 15 minutos
-
-    }else if(duration === -1 && interval){
-
-      document.querySelector('audio').play();
-      stopTimer(true);
-      interval = false;
-
-    }
-
-    display.innerHTML = minutes + ":" + seconds;
-
-  }, 1000);
 }
+
 
 function startTimer(start = false) {
 
-  let display = document.querySelector('.timer > p');
-  let duration = document.querySelector('.options > select').value * 60;
+  let display = document.querySelector(".timer > p");
+  let duration = document.querySelector(".options > select").value * 60;
 
   if (start && !active) {
 
@@ -55,11 +32,37 @@ function startTimer(start = false) {
 
 }
 
-function stopTimer(stop = false) {
 
-  if (stop) {
-    clearInterval(currentTimer);
-    active = false;
-  }
+function timer(duration, display) {
+  let minutes;
+  let seconds;
+  currentTimer = setInterval(function () {
+    minutes = parseInt(duration / 60, 10);
+    seconds = parseInt(duration % 60, 10);
 
+    duration--;
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+
+    if (duration === -1 && !interval) {
+
+      document.querySelector("audio").play();
+      stopTimer(true);
+      interval = true;
+      active = true;
+      timer(900, display); // descanso de 15 minutos
+
+    } else if (duration === -1 && interval) {
+
+      document.querySelector("audio").play();
+      stopTimer(true);
+      interval = false;
+
+    }
+
+    display.textContent = minutes + ":" + seconds;
+
+  }, 1000);
 }
